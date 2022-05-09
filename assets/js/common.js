43566717bc9f41229d783cbee4904202fe7b2713
MyoHtetGitHub <myohtetgithub1996@gmail.com>
2022-05-09 21:32:37 +0630

    uptade js

--- assets/js/common.js
+++ assets/js/common.js
@@ -123,20 +123,31 @@ jQuery(function($) {
     $(window).scroll(function () {
       fadeAnime(); //call function fadeAnime
     });
+    // function fadeAnime() {
+    //   $(".fadeUpTrigger").each(function () {
+    //   //fadeUpTriggerというクラス名が
+    //   var elemPos = $(this).offset().top - 50; //要素より、50px上の
+    //   var scroll = $(window).scrollTop();
+    //   var windowHeight = $(window).height();
+    //   if (scroll >= elemPos - windowHeight) {
+    //     $(this).addClass("fadeUp"); // 画面内に入ったらfadeUpというクラス名を追記
+    //   } else {
+    //     $(this).removeClass("fadeUp"); // 画面外に出たらfadeUpというクラス名を外す
+    //   }
+    // });
+    // };
     function fadeAnime() {
-      $(".fadeUpTrigger").each(function () {
-      //fadeUpTriggerというクラス名が
-      var elemPos = $(this).offset().top - 50; //要素より、50px上の
+    $('.fadeUpTrigger').each(function(){ //fadeUpTriggerというクラス名が
+      var elemPos = $(this).offset().top-50;//要素より、50px上の
       var scroll = $(window).scrollTop();
       var windowHeight = $(window).height();
-      if (scroll >= elemPos - windowHeight) {
-        $(this).addClass("fadeUp"); // 画面内に入ったらfadeUpというクラス名を追記
-      } else {
-        $(this).removeClass("fadeUp"); // 画面外に出たらfadeUpというクラス名を外す
-      }
-    });
-    };
-    
+      if (scroll >= elemPos - windowHeight){
+        $(this).addClass('fadeUp');// 画面内に入ったらfadeUpというクラス名を追記
+    }else{
+        $(this).removeClass('fadeUp');// 画面外に出たらfadeUpというクラス名を外す
+    }
+  });
+  }
     $(function() {
       $('.new-list li').matchHeight();
     });
