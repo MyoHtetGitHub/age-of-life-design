9244319792fb136ea0070794a102e9482b8e7d74
MyoHtetGitHub <myohtetgithub1996@gmail.com>
2022-05-10 20:20:27 +0630

    uptade index

--- assets/js/common.js
+++ assets/js/common.js
@@ -70,73 +70,24 @@ jQuery(function($) {
 
         });
          
-    // function PageTopAnime() {
-    //   var scroll = $(window).scrollTop();
-    //   if (scroll >= 100){
-    //     $('#page-top').removeClass('DownMove');
-    //     $('#page-top').addClass('UpMove');
-    //   }else{
-    //     if($('#page-top').hasClass('UpMove')){
-    //       $('#page-top').removeClass('UpMove');
-    //       $('#page-top').addClass('DownMove');
-    //     }
-    //   }
-    // }
-
-
-    // $(window).scroll(function () {
-    //   PageTopAnime();
-    // });
-
-
-    // $(window).on('load', function () {
-    //   PageTopAnime();
-    // });
-
-    // $('#page-top').click(function () {
-    //   var scroll = $(window).scrollTop();
-    //   if(scroll > 0){
-    //     $(this).addClass('floatAnime');
-    //         $('body,html').animate({
-    //             scrollTop: 0
-    //         }, 2000,function(){
-    //             $('#page-top').removeClass('floatAnime');
-    //         });	
-    //   }
-    //     return false;
-    // });
-    //fade trigger top
-    // function fadeAnime() {
-    //   $(".flipLeftTopTrigger").each(function () {
-    //     //flipLeftTopTriggerというクラス名が
-    //     var elemPos = $(this).offset().top - 50; //要素より、50px上の
-    //     var scroll = $(window).scrollTop();
-    //     var windowHeight = $(window).height();
-    //     if (scroll >= elemPos - windowHeight) {
-    //       $(this).addClass("flipLeftTop"); // 画面内に入ったらflipLeftTopというクラス名を追記
-    //     } else {
-    //       $(this).removeClass("flipLeftTop"); // 画面外に出たらflipLeftTopというクラス名を外す
-    //     }
-    //   });
-    // }
-
-    $(window).scroll(function () {
-      fadeAnime(); //call function fadeAnime
-    });
-    function fadeAnime() {
-      $(".fadeUpTrigger").each(function () {
-      //fadeUpTriggerというクラス名が
-      var elemPos = $(this).offset().top - 20; //要素より、50px上の
-      var scroll = $(window).scrollTop();
-      var windowHeight = $(window).height();
-      if (scroll >= elemPos - windowHeight) {
-        $(this).addClass("fadeUp"); // 画面内に入ったらfadeUpというクラス名を追記
-      } else {
-        $(this).removeClass("fadeUp"); // 画面外に出たらfadeUpというクラス名を外す
-      }
-    });
-    };
-    
+        function fadeAnime() {
+          $(".fadeUpTrigger").each(function () {
+            //fadeUpTriggerというクラス名が
+            var elemPos = $(this).offset().top - 50; //要素より、50px上の
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll >= elemPos - windowHeight) {
+              $(this).addClass("fadeUp"); // 画面内に入ったらfadeUpというクラス名を追記
+            } else {
+              $(this).removeClass("fadeUp"); // 画面外に出たらfadeUpというクラス名を外す
+            }
+          });
+        }
+        $(window).scroll(function () {
+          fadeAnime(); //スクロールに連動した動きの関数を呼ぶ
+        });
+      
+ 
     $(function() {
       $('.new-list li').matchHeight();
     });
